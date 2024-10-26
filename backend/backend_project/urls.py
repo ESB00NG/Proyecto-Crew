094f10bbe9a215d1f8e5a125cb51e3176e145be5
ESB00NG <163206893+ESB00NG@users.noreply.github.com>
2024-10-27 01:52:04 +0200

    correcion url back ahora lleva a admin

--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/')),  # Redirige la raíz a /admin
     path('api/', include('loops.urls')), # ruta prueba
     #path('api/', include('loops.urls')),  # Incluir las rutas de la API
 ]
